9ececd2c99cec8db7e454615ae1be55df0f36764
myenne1 <yennesmuhanad@gmail.com>
2025-03-22 22:23:36 -0500

    deleted section of code

--- uh-surance/src/components/DocumentGetter.jsx
+++ uh-surance/src/components/DocumentGetter.jsx
@@ -11,18 +11,6 @@ export default function DocumentGetter() {
     const handleClick = () => {
         fileInputRef.current.click()
     }
-    async function uploadDatasetFile(formData) {
-        try {
-          const response = await axios.post(
-            "http://localhost:5000/files/upload",
-            formData,
-            {headers: {'Content-Type': 'multipart/form-data'}}
-          );
-          return response.data;
-        } catch (error: any) {
-          throw error.response.data;
-        }
-      }
 
     return (
         <div>
